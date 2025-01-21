1db76ea76468a3bf97177147d83311b089b75ee6
manan.patel <manan.patel@flipkart.com>
2025-01-21 16:38:35 +0530

    Payment and booking beans

---
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/Booking.java
@@ -0,0 +1,51 @@
+package com.flipkart.bean;
+
+import java.util.Date;
+
+public class Booking {
+    private Long bookingId;
+    private Long customerId;
+    private Long slotId;
+    private Date bookingDate;
+    private Boolean bookingStatus;
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(Long slotId) {
+        this.slotId = slotId;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public Boolean getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(Boolean bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+}
--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/Payment.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/Payment.java
@@ -4,18 +4,49 @@ import com.flipkart.utils.PaymentType;
 
 public class Payment {
     private Long paymentId;
+    private Long bookingId;
     private PaymentType paymentType;
     private Long payerId;
+    private Boolean paymentStatus;
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
 
     public Long getPaymentId() {
         return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public Long getPayerId() {
+        return payerId;
+    }
+
+    public void setPayerId(Long payerId) {
+        this.payerId = payerId;
     }
 
     public PaymentType getPaymentType() {
         return paymentType;
     }
 
-    public Long getPayerId() {
-        return payerId;
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType;
     }
+
+    public Boolean getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(Boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
 }
