f1ec72613aeed660842d18236f2ebd8844f92d77
Agrima Gupta <agrima.gupta@flipkart.com>
2025-01-21 22:59:34 +0530

    Adding Admin Operations 2.0

--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/business/AdminOperation.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/business/AdminOperation.java
@@ -1,9 +1,16 @@
 package com.flipkart.business;
 
 import com.flipkart.bean.Admin;
+import com.flipkart.bean.*;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class AdminOperation {
 	Admin admin = new Admin();
+	
+	private List<GymOwner> pendinGymOwnerList = new ArrayList<>();
+    private List<Centre> pendinGymCentreList = new ArrayList<>();
 	
 	public void createAdmin(long adminId, String adminName, String adminEmail, String adminPhone, String password)
 	{
@@ -14,7 +21,34 @@ public class AdminOperation {
 		admin.setPassword(password);
 		return;
 	}
-	// view gym
+	// 1- approve requests  2- view stuffs
+	
+	//TODO - task 1 (approve)
+	
+	public boolean approveGymOwner(long ownerId)
+	{
+		System.out.println("Owner with ID: "+ownerId+" is approved");
+		return true;
+	}
+	
+	public boolean approveGymCenter(long centerId)
+	{
+		System.out.println("Center with ID: "+centerId+" is approved");
+		return true;
+	}
+	
+	//TODO - task 2 (view)
+	
+	public List<Centre> viewPendingGymCentres(){
+        pendinGymCentreList = new ArrayList<>(); // TODO -> get the list
+        return pendinGymCentreList;
+    }
+
+    public List<GymOwner> viewPendingGymOwners(){
+        System.out.println("Viewing pending Gym Owner Approvals: ");
+        pendinGymOwnerList = new ArrayList<>();  //TODO -> get the list
+        return pendinGymOwnerList;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
