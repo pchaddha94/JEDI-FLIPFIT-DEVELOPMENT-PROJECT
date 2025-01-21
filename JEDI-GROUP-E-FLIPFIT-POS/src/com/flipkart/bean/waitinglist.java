09c3b74937fc50200699a9da745c94bb83de94a6
pchaddha94 <parul.chaddha@flipkart.com>
2025-01-21 17:40:41 +0530

    waiting list added

--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/waitinglist.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/waitinglist.java
@@ -1,11 +1,34 @@
-/**
- * 
- */
 package com.flipkart.bean;
-
-/**
- * 
- */
-public class waitinglist {
-
+import java.util.LinkedList;
+import java.util.Queue;
+public class WaitingList {
+	private long centreId;
+	private long waitListId;
+	private int slotId;
+	private Queue<Integer> waiting = new LinkedList<>();
+	public long getCentreId() {
+		return centreId;
+	}
+	public void setCentreId(long centreId) {
+		this.centreId = centreId;
+	}
+	public long getWaitListId() {
+		return waitListId;
+	}
+	public void setWaitListId(long waitListId) {
+		this.waitListId = waitListId;
+	}
+	public int getSlotId() {
+		return slotId;
+	}
+	public void setSlotId(int slotId) {
+		this.slotId = slotId;
+	}
+	public Queue<Integer> getWaiting() {
+		return waiting;
+	}
+	public void setWaiting(Queue<Integer> waiting) {
+		this.waiting = waiting;
+	}
+	
 }
