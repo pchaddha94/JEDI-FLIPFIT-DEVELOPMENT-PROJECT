9246b92d6caf1419b55b11da82b2e0f4a286df2a
Agrima Gupta <agrima.gupta@flipkart.com>
2025-01-21 22:13:07 +0530

    Adding customer and gymOwner props

--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/Customer.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/Customer.java
@@ -1,5 +1,83 @@
 package com.flipkart.bean;
 
 public class Customer {
+	private long customerId;
+	/**
+	 * @return the customerId
+	 */
+	public long getCustomerId() {
+		return customerId;
+	}
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	/**
+	 * @return the customerAddress
+	 */
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	/**
+	 * @param customerAddress the customerAddress to set
+	 */
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	/**
+	 * @return the customerEmailAddress
+	 */
+	public String getCustomerEmailAddress() {
+		return customerEmailAddress;
+	}
+	/**
+	 * @param customerEmailAddress the customerEmailAddress to set
+	 */
+	public void setCustomerEmailAddress(String customerEmailAddress) {
+		this.customerEmailAddress = customerEmailAddress;
+	}
+	/**
+	 * @return the customerPhone
+	 */
+	public int getCustomerPhone() {
+		return customerPhone;
+	}
+	/**
+	 * @param customerPhone the customerPhone to set
+	 */
+	public void setCustomerPhone(int customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String customerName;
+	private String customerAddress;
+	private String customerEmailAddress;
+	private int customerPhone;
+	private String password;
 
 }
--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/GymOwner.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/bean/GymOwner.java
@@ -2,4 +2,95 @@ package com.flipkart.bean;
 
 public class GymOwner {
 
+	private long ownerId;
+	/**
+	 * @return the ownerId
+	 */
+	public long getOwnerId() {
+		return ownerId;
+	}
+	/**
+	 * @param ownerId the ownerId to set
+	 */
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
+	/**
+	 * @return the ownerName
+	 */
+	public String getOwnerName() {
+		return ownerName;
+	}
+	/**
+	 * @param ownerName the ownerName to set
+	 */
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	/**
+	 * @return the ownerEmailAddress
+	 */
+	public String getOwnerEmailAddress() {
+		return ownerEmailAddress;
+	}
+	/**
+	 * @param ownerEmailAddress the ownerEmailAddress to set
+	 */
+	public void setOwnerEmailAddress(String ownerEmailAddress) {
+		this.ownerEmailAddress = ownerEmailAddress;
+	}
+	/**
+	 * @return the ownerPhone
+	 */
+	public int getOwnerPhone() {
+		return ownerPhone;
+	}
+	/**
+	 * @param ownerPhone the ownerPhone to set
+	 */
+	public void setOwnerPhone(int ownerPhone) {
+		this.ownerPhone = ownerPhone;
+	}
+	/**
+	 * @return the ownerGSTNum
+	 */
+	public int getOwnerGSTNum() {
+		return ownerGSTNum;
+	}
+	/**
+	 * @param ownerGSTNum the ownerGSTNum to set
+	 */
+	public void setOwnerGSTNum(int ownerGSTNum) {
+		this.ownerGSTNum = ownerGSTNum;
+	}
+	/**
+	 * @return the isApproved
+	 */
+	public boolean isApproved() {
+		return isApproved;
+	}
+	/**
+	 * @param isApproved the isApproved to set
+	 */
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
+	/**
+	 * @return the ownerAddress
+	 */
+	public String getOwnerAddress() {
+		return ownerAddress;
+	}
+	/**
+	 * @param ownerAddress the ownerAddress to set
+	 */
+	public void setOwnerAddress(String ownerAddress) {
+		this.ownerAddress = ownerAddress;
+	}
+	private String ownerName;
+	private String ownerEmailAddress;
+	private int ownerPhone;
+	private int ownerGSTNum;
+	private boolean isApproved;
+	private String ownerAddress;
 }
