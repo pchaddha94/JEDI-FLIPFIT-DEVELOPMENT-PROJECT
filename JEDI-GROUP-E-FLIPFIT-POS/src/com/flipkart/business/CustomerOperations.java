2f2ca6764baefdfa921ceacbd66c6871b4ec2176
sangwanseema <seema.s1@flipkart.com>
2025-01-21 23:03:23 +0530

    Added CustomerOperations.java

---
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/business/CustomerOperations.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+/**
+ * 
+ */
+public class CustomerOperations {
+     public void createCustomer (Long customerId, String customerName,String customerAddress, String customerEmail, int customerPhone , String customerPassword )
+     {
+    	 // create Customer 
+    	 return;
+      
+     }
+     public void bookSlot(String Gym)
+     {
+    	 // Book slot
+    	 System.out.println("Book Slot");
+    	 return;
+     }
+     public void searchGym(String location)
+     {
+    	 // search Gym
+    	 System.out.println("Search Gym");
+    	 return;
+     }
+     public void viewAllBooking(Long customerId)
+     {
+    	 // view All Booking
+    	 System.out.println("view All Booking");
+    	 return;
+     }
+     public void cancelSlot()
+     {
+    	// cancel slot
+    	 System.out.println("Cancel Slot");
+    	 return ;
+     }
+     public void updateSlot()
+     {
+    	 //Update slot
+    	 System.out.println("Update Slot");
+    	 return ;
+     }
+     public void editCustomerProfile(Long customerId)
+     {
+    	 // Edit Customer Details
+    	 System.out.println("Edit Customer Details");
+    	 return;
+     }
+}
