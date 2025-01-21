83a5457917b01e9986218583d3ff0be0f5dfe9d7
Saksham Bassi <saksham.bassi@flipkart.com>
2025-01-21 23:03:25 +0530

    feedback operation added

--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/business/FeedbackOperation.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/business/FeedbackOperation.java
@@ -6,22 +6,25 @@ import java.util.List;
 public class FeedbackOperation {
 
     public void addFeedback(Long userId, String comments, int rating) {
-
+        System.out.println("Adding feedback for User ID: " + userId);
     }
 
     public void viewAllFeedback() {
-
+        System.out.println("Fetching all feedback...");
     }
 
     public List<Feedback> getFeedbackByUserId(Long userId) {
+        System.out.println("Fetching feedback for User ID: " + userId);
         return null;
     }
 
     public double getAverageRating() {
+        System.out.println("Calculating average rating...");
         return 0.0;
     }
 
     public boolean deleteFeedback(Long feedbackId) {
+        System.out.println("Deleting feedback with Feedback ID: " + feedbackId);
         return false;
     }
 }
