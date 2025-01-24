9cc6acdf59a2b234e18ec4fadf6463a08b582ed7
manan.patel <manan.patel@flipkart.com>
2025-01-24 12:03:30 +0530

    Exception message added

--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/exceptions/NoSlotsFoundException.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/exceptions/NoSlotsFoundException.java
@@ -1,5 +1,7 @@
 package com.flipkart.exceptions;
 
 public class NoSlotsFoundException extends Exception {
-
+    public String getMessage() {
+        return "No slot found";
+    }
 }
--- JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/exceptions/UserNotFoundException.java
+++ JEDI-GROUP-E-FLIPFIT-POS/src/com/flipkart/exceptions/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.flipkart.exceptions;
 
 public class UserNotFoundException extends Exception {
-
+    public String getMessage() {
+        return "User not found";
+    }
 }
